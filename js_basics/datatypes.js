afe13bea3554fe957b2c43c6311a53aa43cc9ad5
tawheedullahh <tawheedullah267@gmail.com>
2026-03-08 23:28:35 +0500

    lil progresss

---
+++ js_basics/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof "Tawheed")
+
+console.log(typeof null)//-> object
+console.log(typeof undefined)//-> undefined
+
---
+++ js_basics/variable.js
@@ -0,0 +1,7 @@
+const studentName = "Tawheed"
+let studentId = 43142
+var studentGPA = 3.89
+
+console.log(studentName);
+console.table ([studentName, studentId, studentGPA])
+
