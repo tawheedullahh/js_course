e346b743f3e3cb2bb74c02d1ec12ed80f185660c
tawheedullahh <tawheedullah267@gmail.com>
2026-03-12 12:25:29 +0500

    iterations done- ezzz

---
+++ medium/iterations.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <=10; i++) {
+    const element =[i];
+    //console.log(i);
+    
+};
+
+for (let i = 1; i <= 10; i++) {
+    const element =[i];
+    console.log("Table of: ", i);
+    for (let j = 1; j <= 10; j++) {
+        const element = [j];
+        console.log(`${i} * ${j} = ${i*j}`);
+    }
+        }
